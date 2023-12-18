27b288632467f0a93b76a83a365be762f4d62519
Bradford Toney <bradford.toney@gmail.com>
2023-12-18 17:36:33 -0500

    fix: exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,15 @@
+import { createURLStore, typedParams } from "$lib/url.js";
 import { queryNumberArray, queryStringArray } from "$lib/utils.js";
+import { type TypedURLStore, type TypedParams } from "$lib/url.js";
 
 export {
+  createURLStore,
   queryNumberArray,
-  queryStringArray
+  queryStringArray,
+  typedParams
+}
+
+export type {
+  TypedURLStore,
+  TypedParams
 }
