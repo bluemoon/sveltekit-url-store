38a7d5ce2bdd952fcea858d6dd197ac0304dd86e
Bradford Toney <bradford.toney@gmail.com>
2023-12-18 17:40:51 -0500

    fix: lint

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,7 @@
-import { createURLStore, typedParams } from "$lib/url.js";
-import { queryNumberArray, queryStringArray } from "$lib/utils.js";
-import { type TypedURLStore, type TypedParams } from "$lib/url.js";
+import { createURLStore, typedParams } from '$lib/url.js';
+import { queryNumberArray, queryStringArray } from '$lib/utils.js';
+import { type TypedURLStore, type TypedParams } from '$lib/url.js';
 
-export {
-  createURLStore,
-  queryNumberArray,
-  queryStringArray,
-  typedParams
-}
+export { createURLStore, queryNumberArray, queryStringArray, typedParams };
 
-export type {
-  TypedURLStore,
-  TypedParams
-}
+export type { TypedURLStore, TypedParams };
